ea62b6a5ecd6fa0c492dffec60b66cf47da8499a
Andy Fillebrown <afillebrown@microsoft.com>
2022-08-18 01:20:12 -0400

    Add CompileShader(...) and call it in NativeCameraImpl.mm Open(...)
    Copied from XR.mm.

--- Plugins/NativeCamera/Source/Apple/NativeCameraImpl.mm
+++ Plugins/NativeCamera/Source/Apple/NativeCameraImpl.mm
@@ -27,6 +27,99 @@
 
 namespace Babylon::Plugins
 {
+    namespace {
+        // This shader is used to render *either* the camera texture or the final composited texture.
+        // It could be split into two shaders, but this is a bit simpler since they use the same structs
+        // and have some common logic.
+        // The shader is used in two passes:
+        // 1. Render the camera texture to the color render texture (see GetNextFrame).
+        // 2. Render the composited texture to the screen (see DrawFrame).
+        //
+        // NB: This is a copy/paste from Dependencies/xr/Source/ARKit/XR.mm:565
+        //
+        // NB: Use this shader as a reference for the BGRA to RGBA conversion.
+        //      - The pixel channels might not need to be swapped at all. A straight copy from one texture to the other might do the swap automatically based on the texture formats.
+        //      - Use Y CbCr camera format. (This shader does the conversion from Y CbCr to RGBA).
+        //
+        constexpr char shaderSource[] = R"(
+            #include <metal_stdlib>
+            #include <simd/simd.h>
+
+            using namespace metal;
+
+            #include <simd/simd.h>
+
+            typedef struct
+            {
+                vector_float2 position;
+                vector_float2 uv;
+                vector_float2 cameraUV;
+            } XRVertex;
+
+            typedef struct
+            {
+                float4 position [[position]];
+                float2 uv;
+                float2 cameraUV;
+            } RasterizerData;
+
+            vertex RasterizerData
+            vertexShader(uint vertexID [[vertex_id]],
+                         constant XRVertex *vertices [[buffer(0)]])
+            {
+                RasterizerData out;
+                out.position = vector_float4(vertices[vertexID].position.xy, 0.0, 1.0);
+                out.uv = vertices[vertexID].uv;
+                out.cameraUV = vertices[vertexID].cameraUV;
+                return out;
+            }
+
+            fragment float4 fragmentShader(RasterizerData in [[stage_in]],
+                texture2d<float, access::sample> babylonTexture [[ texture(0) ]],
+                texture2d<float, access::sample> cameraTextureY [[ texture(1) ]],
+                texture2d<float, access::sample> cameraTextureCbCr [[ texture(2) ]])
+            {
+                constexpr sampler linearSampler(mip_filter::linear, mag_filter::linear, min_filter::linear);
+
+                if (!is_null_texture(babylonTexture))
+                {
+                    return babylonTexture.sample(linearSampler, in.uv);
+                }
+                else if (!is_null_texture(cameraTextureY) && !is_null_texture(cameraTextureCbCr))
+                {
+                    const float4 cameraSampleY = cameraTextureY.sample(linearSampler, in.cameraUV);
+                    const float4 cameraSampleCbCr = cameraTextureCbCr.sample(linearSampler, in.cameraUV);
+
+                    const float4x4 ycbcrToRGBTransform = float4x4(
+                        float4(+1.0000f, +1.0000f, +1.0000f, +0.0000f),
+                        float4(+0.0000f, -0.3441f, +1.7720f, +0.0000f),
+                        float4(+1.4020f, -0.7141f, +0.0000f, +0.0000f),
+                        float4(-0.7010f, +0.5291f, -0.8860f, +1.0000f)
+                    );
+
+                    float4 ycbcr = float4(cameraSampleY.r, cameraSampleCbCr.rg, 1.0);
+                    float4 cameraSample = ycbcrToRGBTransform * ycbcr;
+                    cameraSample.a = 1.0;
+
+                    return cameraSample;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        )";
+
+        id<MTLLibrary> CompileShader(id<MTLDevice> metalDevice, const char* source) {
+            NSError* error;
+            id<MTLLibrary> lib = [metalDevice newLibraryWithSource:@(source) options:nil error:&error];
+            if(nil != error) {
+                throw std::runtime_error{[error.localizedDescription cStringUsingEncoding:NSASCIIStringEncoding]};
+            }
+            return lib;
+        }
+    }
+
     struct Camera::Impl::ImplData
     {
         ~ImplData()
@@ -63,6 +156,12 @@ namespace Babylon::Plugins
         auto metalDevice = (id<MTLDevice>)bgfx::getInternalData()->context;
         auto commandQueue = (id<MTLCommandQueue>)bgfx::getInternalData()->commandQueue;
         (void)commandQueue;
+
+        id<MTLLibrary> lib = CompileShader(metalDevice, shaderSource);
+        id<MTLFunction> vertexFunction = [lib newFunctionWithName:@"vertexShader"];
+        id<MTLFunction> fragmentFunction = [lib newFunctionWithName:@"fragmentShader"];
+        (void)vertexFunction;
+        (void)fragmentFunction;
 
         if (!m_deviceContext) {
             m_deviceContext = &Graphics::DeviceContext::GetFromJavaScript(m_env);
