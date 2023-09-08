65e67626841b7002832f13602a061c31391132d1
Andy Fillebrown <andy@docEdub.com>
2023-09-08 12:11:04 -0400

    Remove internal AudioContext.h

--- Polyfills/AudioContext/Source/AudioContext.cpp
+++ Polyfills/AudioContext/Source/AudioContext.cpp
@@ -1,29 +1,35 @@
-#include "AudioContext.h"
+#include <Babylon/JsRuntime.h>
+
+#include <napi/napi.h>
 
 namespace Babylon::Polyfills::Internal
 {
-    namespace
+    class AudioContext : public Napi::ObjectWrap<AudioContext>
     {
-        constexpr auto JS_CONSTRUCTOR_NAME = "AudioContext";
-    }
+        static constexpr auto JS_CLASS_NAME = "AudioContext";
 
-    void AudioContext::Initialize(Napi::Env env)
-    {
-        Napi::HandleScope scope{env};
+    public:
+        static void Initialize(Napi::Env env)
+        {
+            Napi::HandleScope scope{env};
 
-        Napi::Function func = DefineClass(
-            env,
-            JS_CONSTRUCTOR_NAME,
-            {});
+            Napi::Function func = DefineClass(
+                env,
+                JS_CLASS_NAME,
+                {});
 
-        env.Global().Set(JS_CONSTRUCTOR_NAME, func);
-    }
+            env.Global().Set(JS_CLASS_NAME, func);
+        }
 
-    AudioContext::AudioContext(const Napi::CallbackInfo& info)
-        : Napi::ObjectWrap<AudioContext>{info}
-        , m_runtime{JsRuntime::GetFromJavaScript(info.Env())}
-    {
-    }
+        AudioContext(const Napi::CallbackInfo& info)
+            : Napi::ObjectWrap<AudioContext>{info}
+            , m_runtime{JsRuntime::GetFromJavaScript(info.Env())}
+        {
+        }
+
+    private:
+        JsRuntime& m_runtime;
+    };
 }
 
 namespace Babylon::Polyfills::AudioContext
