dbbcde69b95b8e797c60dbf9621103b152ac2e3b
Andy Fillebrown <afillebrown@microsoft.com>
2023-09-11 18:56:08 -0400

    Re-enable tests and add more tests

--- Apps/UnitTests/Scripts/audio-tests.js
+++ Apps/UnitTests/Scripts/audio-tests.js
@@ -2,37 +2,43 @@
 
 const expect = chai.expect;
 
-xdescribe("AudioContext", function () {
+describe("AudioContext", function () {
     this.timeout(0);
 
     it("is constructable", function () {
         const audioContext = new AudioContext();
+        expect(audioContext).to.not.be.null;
         expect(audioContext).to.not.be.undefined;
+        expect(audioContext instanceof AudioContext).to.be.true;
     });
 
     it("create a gain node", function () {
         const audioContext = new AudioContext();
-        // const gainNode = audioContext.createGain();
-        // expect(gainNode).to.not.be.undefined;
+        const gainNode = audioContext.createGain();
+        expect(gainNode).to.not.be.null;
+        expect(gainNode).to.not.be.undefined;
+        expect(gainNode instanceof GainNode).to.be.true;
     });
 });
 
 describe("GainNode", function () {
     this.timeout(0);
 
-    // it("is constructable", function () {
-    //     const audioContext = new AudioContext();
-    //     const gainNode = new GainNode(audioContext);
-    //     expect(gainNode).to.not.be.undefined;
-    // });
+     it("is constructable", function () {
+         const audioContext = new AudioContext();
+         const gainNode = new GainNode(audioContext);
+         expect(gainNode).to.not.be.undefined;
+     });
+
+    it("is instanceof AudioNode", function () {
+        const audioContext = new AudioContext();
+        const gainNode = new GainNode(audioContext)
+        expect(gainNode instanceof AudioNode).to.be.true;
+    });
 
     it("connects to audio context destination", function () {
         const audioContext = new AudioContext();
-
         const gainNode = new GainNode(audioContext);
-
-        console.log(gainNode instanceof AudioNode);
-
         const connectedNode = gainNode.connect(audioContext.destination);
         expect(connectedNode).to.equal(audioContext.destination);
     });
