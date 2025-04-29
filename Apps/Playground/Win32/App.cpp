b6333b603f11ddecd44e55bd749dd148ddb0504f
Andy Fillebrown <afillebrown@microsoft.com>
2025-04-29 10:40:33 -0400

    Use app.js built from 250428-smartfilters-playground
    (cherry picked from commit d614de089936450f5390a9645b8a0470fae1ec0a)

--- Apps/Playground/Win32/App.cpp
+++ Apps/Playground/Win32/App.cpp
@@ -199,19 +199,19 @@ namespace
         });
 
         Babylon::ScriptLoader loader{*runtime};
-        loader.LoadScript("app:///Scripts/ammo.js");
+        // loader.LoadScript("app:///Scripts/ammo.js");
         // Commenting out recast.js for now because v8jsi is incompatible with asm.js.
         // loader.LoadScript("app:///Scripts/recast.js");
-        loader.LoadScript("app:///Scripts/babylon.max.js");
-        loader.LoadScript("app:///Scripts/babylonjs.loaders.js");
-        loader.LoadScript("app:///Scripts/babylonjs.materials.js");
-        loader.LoadScript("app:///Scripts/babylon.gui.js");
-        loader.LoadScript("app:///Scripts/meshwriter.min.js");
+        // loader.LoadScript("app:///Scripts/babylon.max.js");
+        // loader.LoadScript("app:///Scripts/babylonjs.loaders.js");
+        // loader.LoadScript("app:///Scripts/babylonjs.materials.js");
+        // loader.LoadScript("app:///Scripts/babylon.gui.js");
+        // loader.LoadScript("app:///Scripts/meshwriter.min.js");
 
         std::vector<std::string> scripts = GetCommandLineArguments();
         if (scripts.empty())
         {
-            loader.LoadScript("app:///Scripts/experience.js");
+            loader.LoadScript("app:///Scripts/app.js");
         }
         else
         {
